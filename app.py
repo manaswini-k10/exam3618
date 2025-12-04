bf851824aa2a39c067eeed2fd0559de28af599d5
manaswini-k10 <manaswinikuchur@gmail.com>
2025-12-04 11:24:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask,render_template,request
-app= Flask(__name__)
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
 @app.route('/')
 def register():
     return render_template('registration.html')
+
 @app.route('/greet', methods=['POST'])
 def greet():
     name = request.form['name']
-    email= request.form['email']
-    return render_template('greetings.html',name=name,email=email)
-if __name__=="__main__":
-    app.run('POST = 0.0.0.0', port = 5000, debug = True)
+    email = request.form['email']
+    return render_template('greetings.html', name=name, email=email)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
